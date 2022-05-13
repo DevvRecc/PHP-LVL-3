19fbf9bbbb6f9fea2f6edafaa5a907959b3f6f9e
Recep Yildiz <recep.yildiz@student.rocva.nl>
2022-05-13 11:44:37 +0200

    08 gemaakt

---
+++ O8-recep.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>O8-Recep</title>
+</head>
+<body>
+
+<table border=1>
+    <?php 
+        for($y=1; $y <13;$y++){
+            echo "<tr>";
+                for($r=1; $r<11;$r++){
+                    echo "<td>$r</td>";
+                }
+                echo "</tr>";
+            }
+    ?>
+
+</table>
+
+</body>
+</html>
